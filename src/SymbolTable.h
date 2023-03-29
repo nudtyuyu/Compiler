8cc9d9d717fc694fe644af6bc330e5557705c5b8
Rhein-E <1308498716@qq.com>
2023-03-29 12:47:58 +0800

    朴素的符号表

--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -61,3 +61,29 @@ public:
 
 } // namespace sysy
 
+/*
+#include"SymbolTable.h"
+#include "IR.h"
+int main() {
+    sysy::SymbolTable table;
+
+    // 一级符号表（全局）
+    table.newTable();
+    table.insert("x", (sysy::Value *) 0x1000080);
+    table.insert("func", (sysy::Value *) 0x4000068);
+
+    // 二级符号表（函数）
+    table.newTable();
+    table.insert("abc", (sysy::Value *) 0x8000040);
+    table.insert("w", (sysy:: Value *) 0x8000008);
+    
+    table.view();
+
+    // 查询符号表
+    auto *ptr = table.query("abc");
+    std::cout << "The pointer of \"abc\" is : ";
+    std::cout << std::hex << std::showbase << (unsigned long long)ptr << "\n";
+
+    return 0;
+}
+*/
