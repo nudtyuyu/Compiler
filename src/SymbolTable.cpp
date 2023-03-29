60abb7fde08823c3fa943f30ccd94b85ffe6495b
Rhein-E <1308498716@qq.com>
2023-03-29 12:43:50 +0800

    朴素的符号表

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -4,36 +4,54 @@
  * @author Rhein_E
  * @version 1.0
  * @date 2023-03-28
- * 
+ *
  * @details 通过std::map将符号的名字(std::string)映射到指向Value对象的指针。
  */
 #include "SymbolTable.h"
 
 namespace sysy {
 
-bool SymbolTable::insert(const std::string &name, const Value *ptr) {
-    if (table.find(name) != table.end())
+bool SymbolTable::insert(const std::string &name, Value *ptr) {
+    auto &topTable = *table.back();
+    if (topTable.find(name) != topTable.end())
         return false;
-    table[name] = ptr;
+    topTable[name] = ptr;
     return true;
 }
 
 Value *SymbolTable::query(const std::string &name) {
-    if (table.find(name) != table.end())
-        return table[name];
+    for (auto it = table.rbegin(); it != table.rend(); ++it)
+        if ((*it)->find(name) != (*it)->end())
+            return (*it)->at(name);
     return nullptr;
 }
 
-void SymbolTable::view() const {
-    std::cout << std::right;
-    std::cout << "|" << std::setw(15) << "Name"
-              << "|" << std::setw(15) << "Address"
-              << "|\n";
-    for (auto &pair : table) {
-        std::cout << "|" << std::setw(15) << pair.first
-        std::cout << "|" << std::setw(15) << std::hex << std::showbase << (unsigned int)pair.second->addr << "|\n";
+void SymbolTable::newTable() {
+    table.push_back(new SubTable());
+}
+
+void SymbolTable::destroyTop() {
+    if (!table.empty()) {
+        delete table.back();
+        table.pop_back();
     }
-    std::cout.unsetf(std::cout.flags());
+}
+
+void SymbolTable::view() const {
+    for (auto *pSubTable : table) {
+        std::cout << "\n+---------------+---------------+\n";
+        std::cout << std::right;
+        std::cout << "|" << std::setw(15) << "Name"
+                  << "|" << std::setw(15) << "Address"
+                  << "|\n";
+        std::cout << "+---------------+---------------+\n";
+        for (auto &pair : *pSubTable) {
+            std::cout << "|" << std::setw(15) << pair.first;
+            std::cout << "|" << std::setw(15) << std::hex << std::showbase << (unsigned long long)pair.second << "|\n";
+        }
+        std::cout.unsetf(std::cout.flags());
+        std::cout << "+---------------+---------------+\n";
+    }
 }
 
 } // namespace sysy
--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -4,37 +4,53 @@
  * @author Rhein_E
  * @version 1.0
  * @date 2023-03-28
+ * 
+ * @details 以栈的方式组织多个符号表，每个符号表对应一个域。
+ * 使用时只需要创建一个SymbolTable类。完整的示例见文件末尾。
  */
 #include <iostream>
 #include <map>
 #include <string>
 #include <iomanip>
+#include <vector>
 
 #include "IR.h"
 
 namespace sysy {
 
 class SymbolTable {
+using SubTable=std::map<std::string, Value *>;
 private:
-    std::map<std::string, Value *> table;
+    std::vector<SubTable *> table;
 
 public:
     /**
      * @brief 插入符号
-     * @param name 符号名字
+     * @param[in] name 符号名字
      * @return true 插入成功 \\
      * @return false 插入失败，通常表明有名字冲突
      * @author Rhein_E
+     * @example table.insert("x", (sysy::Value *) 0x1000080);
      */
-    bool insert(const std::string &name, const Value *ptr);
+    bool insert(const std::string &name, Value *ptr);
 
     /**
      * @brief  查询符号
-     * @param name 符号的名字
+     * @param[in] name 符号的名字
      * @return 返回一个指向Value对象的指针，若为nullptr表示没有找到对应名字的符号
      * @author Rhein_E
      */
     Value *query(const std::string &name);
+
+    /**
+     * @brief 创建一个子表并入栈
+     */
+    void newTable();
+
+    /**
+     * @brief 销毁栈顶符号表
+     */
+    void destroyTop();
 
     /**
      * @brief 打印符号表（调试使用）
