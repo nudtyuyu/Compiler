a5a6a03b2b4bb152921281c05cc864c4b021f579
Rhein-E <1308498716@qq.com>
2023-04-09 22:57:26 +0800

    git track更新

---
+++ antlr/antlr4-runtime/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rhein-e/workspace/course/并行编译/Compiler/antlr/antlr4-runtime
+# Build directory: /home/rhein-e/workspace/course/并行编译/Compiler/antlr/antlr4-runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("runtime")
---
+++ antlr/antlr4-runtime/runtime/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rhein-e/workspace/course/并行编译/Compiler/antlr/antlr4-runtime/runtime
+# Build directory: /home/rhein-e/workspace/course/并行编译/Compiler/antlr/antlr4-runtime/runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
